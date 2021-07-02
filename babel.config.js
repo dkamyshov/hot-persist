465d3f53263f4e20aa78567a24a197445734fc62
dkamyshov <danil.kamyshov@gmail.com>
2021-07-02 19:57:54 +0300

    chore: add babel plugins so tests can run on old node.js

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   presets: ['@babel/preset-typescript'],
-  plugins: ['@babel/plugin-transform-modules-commonjs'],
+  plugins: [
+    '@babel/plugin-transform-modules-commonjs',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+  ],
 };
